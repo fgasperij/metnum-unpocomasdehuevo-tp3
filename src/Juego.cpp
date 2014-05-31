a6c3113c92d8fc80aa37f9715161ece1a08ddaae
Franco <franco@ubuntu.(none)>
2014-05-31 14:45:52 -0300

    Compilable y testeado

--- src/Juego.cpp
+++ src/Juego.cpp
@@ -3,7 +3,7 @@
 int Juego::dameTiempo(){return this->timestamp;}
 
 Posicion Juego::damePosicion(int tiempo){
-	if(!(this->flag_test) && tiempo < timestamp+1){	// O timestamp, no estoy seguro
+	if(!(this->flag_test) && timestamp < tiempo){   // Verificar el timestamp < tiempo
 		cerr << "No se puede conocer el futuro " << endl;
         Posicion trucha; trucha.pos_x = -1; trucha.pos_y = -1;
 		return trucha;
@@ -18,7 +18,7 @@ void Juego::avanzarJuego(){(this->timestamp)++;}
 void Juego::setearFlagTest(bool status){this->flag_test = status;}
 
 bool Juego::terminoJuego(){
-	return (this->timestamp == CANT_MOVIMIENTOS-1);	// O CANT_MOVIMIENTOS, no estoy seguro
+	return (this->timestamp == CANT_MOVIMIENTOS-1);	// o CANT_MOVIMIENTOS, no estoy seguro
 }
 
 double Juego::dameDesplMax(){return this->data->mu;}
