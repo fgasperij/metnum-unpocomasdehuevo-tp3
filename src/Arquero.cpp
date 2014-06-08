b60748375e93ac9d01abf18b4c112da45b8ef9b8
anizis <anizis@users.noreply.github.com>
2014-06-09 01:13:14 +0200

    atajar

--- src/Arquero.cpp
+++ src/Arquero.cpp
@@ -1,6 +1,9 @@
 #include "Arquero.h"
 
 void Arquero::atajar(double posicionEntrada){
+	double posicionPrevia = posicionActual;
+	double desplazamiento=0;
+	// Si la pelota no está al alcance del arquero
 	if ( posicionEntrada > (posicionActual+amplitudMovimiento) )
 	{
 		posicionActual+=amplitudMovimiento;
@@ -11,5 +14,16 @@ void Arquero::atajar(double posicionEntrada){
 		posicionActual-=amplitudMovimiento;
 		if (posicionActual<limiteInferior)
 			posicionActual=limiteInferior;
+	}else 
+	{
+		// Si la pelota está al alcance del arquero, podríamos acercarnos al punto de entrada
 	}
+	// Calculo cuál fue el desplazamiento
+	if (posicionActual>posicionPrevia)
+		desplazamiento=posicionActual-posicionPrevia;
+	else if (posicionActual<posicionPrevia)
+		desplazamiento=-(posicionPrevia-posicionActual);
+		
+	//return desplazamiento;
+	// o bien: movimientos(push_back,desplazamiento);
 }
