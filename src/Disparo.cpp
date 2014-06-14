6a686db2c2ad176e7d736151788f6970c4ce561d
Franco <franco@ubuntu.(none)>
2014-06-14 18:27:44 -0300

    Mejorando

--- src/Disparo.cpp
+++ src/Disparo.cpp
@@ -39,55 +39,44 @@ double Disparo::estimarPorDondePasa() {
 	 *    	Aproximo con grado 0 y 1. Grado 2 ya me daría un interpolador.
 	 */
 
-	// Cantidad de puntos medidos hasta el instante actual.
-	unsigned int cantPuntosActuales = trayectoriaActual.size();
-
-	// En el primer instante devuelvo lo mismo.
-    //if(cantPuntosActuales == 1){return trayectoria[0].y;}
-
-    unsigned int grados = MAX_GRADO;    // Uso polinomios de hasta grado MAX_GRADO.
-
-
 	vector<double> xs = obtenerXs(trayectoriaActual);
     vector<double> ys = obtenerYs(trayectoriaActual);
 
     // Me quedo con la ultima seria de mediciones decrecientes.
     vector<double> aux = xs;
     reverse(aux.begin(), aux.end());
-    vector<double> xsp;
+    unsigned int cantCrecientes = 0;
     for(unsigned int i = 0; i < aux.size() ; i++){
         if(i+1 < aux.size() && aux[i] <= aux[i+1] ){
-            xsp.push_back(aux[i]);
+            cantCrecientes++;
         }
-        else if(i+1 == aux.size()){xsp.push_back(aux[i]);}
+        else if(i+1 == aux.size()){cantCrecientes++;}
         else{break;}
     }
-    // Esto es para que solo considere las ultimas CANT_MEDICIONES. No mejoro eficacia.
-    //if(xsp.size() > CANT_MEDICIONES){xsp.resize(CANT_MEDICIONES);}
-    reverse(xsp.begin(), xsp.end());
-
 
     // Las aproximaciones para los distintos polinomios.
 	vector<aproximacion> aproximaciones(0);
+
     // Con grado 0, la aproximacion es constante. Muy poco util. Aunque cuando hay un solo punto a considerar quizas sea lo mejor.
     aproximaciones.push_back( aproximacion(ys[ys.size()-1]));
 
     // El resto de las aproximaciones hasta polinomios de hasta grado grados. El grado maximo tambien queda definido
     // por la cantidad de puntos actuales.
-	for (unsigned int i = 1; i < xsp.size(); i++) {
 
-        // Esta cosa fea es para que solo considere los ultimos puntos, invierto, me quedo con los primeros y vuelvo a invertir.
-        vector<double> xss = xs;
-        reverse(xss.begin(), xss.end());
-        xss.resize(i+1);
-        reverse(xss.begin(), xss.end());
+    // Esta cosa fea es para que solo considere los ultimos CANT_MEDICIONES puntos positivos, invierto, me quedo con los primeros y vuelvo a invertir.
+    int puntosAConsiderar = min(CANT_MEDICIONES, min((int) trayectoriaActual.size(), (int) cantCrecientes));
+    vector<double> xss = xs;
+    reverse(xss.begin(), xss.end());
+    xss.resize(puntosAConsiderar);
+    reverse(xss.begin(), xss.end());
+    vector<double> yss = ys;
+    reverse(yss.begin(), yss.end());
+    yss.resize(puntosAConsiderar);
+    reverse(yss.begin(), yss.end());
+    // Aproximo con polinomios de hasta grado i.
+	for (unsigned int i = 1; i < cantCrecientes && i < CANT_MEDICIONES; i++) {
 
 		vector<double> coeficientesMinimizadoresXs = minimizarConGrado(xss, i, instanteActual);
-
-        vector<double> yss = ys;
-        reverse(yss.begin(), yss.end());
-        yss.resize(i+1);
-        reverse(yss.begin(), yss.end());
 
 		vector<double> coeficientesMinimizadoresYs = minimizarConGrado(yss, i, instanteActual);
 
@@ -112,12 +101,13 @@ double Disparo::estimarPorDondePasa() {
 	estimaciones.push_back(estimacionPorPolinomio);
 
 
-//	double aproximacionFinal = decidirEnBaseATodasLasAproximaciones(aproximaciones);
+//	decidirEnBaseATodasLasAproximaciones(aproximaciones);
 
 // Por el momento me quedo con la ultima aproximacion valida. Deberia de ser la mas precisa.
     double aproximacionFinal = 0;
+    if(aproximaciones.size() == 1){return aproximaciones[0].valor;}
     double cantAproxs = 0;
-	for(unsigned int i = 0; i < aproximaciones.size(); i++){
+	for(unsigned int i = 1; i < aproximaciones.size(); i++){
         if(aproximaciones[i].valida){
             aproximacionFinal += aproximaciones[i].valor;
             cantAproxs++;
