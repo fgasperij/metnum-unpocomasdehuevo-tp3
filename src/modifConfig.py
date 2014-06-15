4787ec5ec5df3649c64fd8bd522515c768a0c65c
Franco <franco@ubuntu.(none)>
2014-06-15 18:42:08 -0300

    Falto el .py

---
+++ src/modifConfig.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+from sys import argv
+import argparse
+import xml.etree.ElementTree as ET
+
+MAX_GRADO = "max_grado"
+CANT_MEDICIONES = "cant_mediciones"
+
+from xml.dom.minidom import *
+
+def main(arg = argv):
+	parser = argparse.ArgumentParser(description="Archivo para modificar parametros de configuracion global")
+	parser.add_argument("-cm", help="Cantidad de mediciones (ultimas) a tomar, default=None", type=int, default=None)
+	parser.add_argument("-mg", help="Maximo grado a tomar, default=None", type=int, default=None)
+	args = parser.parse_args()
+
+	dom = parse('conf.xml')
+	config = dom.documentElement
+	if(args.cm != None and args.cm > 0):
+		for child in config.getElementsByTagName(CANT_MEDICIONES):
+			config.removeChild(child)
+		cm_node = dom.createElement(CANT_MEDICIONES)
+		cm_text_node = dom.createTextNode(str(args.cm))
+		cm_node.appendChild(cm_text_node)
+		config.appendChild(cm_node)
+
+	if(args.mg != None and args.mg > 0):
+		for child in config.getElementsByTagName(MAX_GRADO):
+			config.removeChild(child)
+		cm_node = dom.createElement(MAX_GRADO)
+		cm_text_node = dom.createTextNode(str(args.mg))
+		cm_node.appendChild(cm_text_node)
+		config.appendChild(cm_node)
+
+	config_file = open("config.xml", 'w')
+	config_file.write(dom.toprettyxml())
+	config_file.close()
+
+if __name__ == "__main__":
+    main()
