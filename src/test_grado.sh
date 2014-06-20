3cf8ef3a4467347fd37b8de5cd485bfd1a10d4c3
Franco <franco@ubuntu.(none)>
2014-06-20 12:10:15 -0300

    Tes grados.sh

---
+++ src/test_grado.sh
@@ -0,0 +1,15 @@
+grados=(1 2 3 4 5 6 7 8 9 10)
+mediciones=(3 6 9 12)
+
+
+for grado in "${grados[@]}"
+do
+	for medicion in "${mediciones[@]}"
+	do
+		echo "Grado: "
+		echo $grado
+		echo "Cantidad de mediciones: "
+		echo $medicion
+		./run_test_fast.sh		
+	done
+done
